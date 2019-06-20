9ef616a4ff16b4c151cfe1c29550f636526f410d
Akshay Gulhane <akshay.gulhane@opcito.com>
2019-06-20 15:45:49 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require('axios');
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:2369/ghost/api/v2/content/posts?key=c55ee6957c081a5a9b99e57c65')
+      .then(response => {
+        console.log(response);
+        let posts = response.data.posts;
+        this.setState({
+          posts
+        })
+      })
+  }
+  render() {
+    let posts_data = this.state.posts.map(post => {
+      return <div dangerouslySetInnerHTML={__html: post.html} />
+    })
+
+    return (  
+      <div className="App">
+        <header className="App-header">
+          <html>{posts_data}</html>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
