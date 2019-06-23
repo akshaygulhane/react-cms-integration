51b255bc361af70e98aff72bfe306268840f08c5
Akshay Gulhane <akshay.gulhane@opcito.com>
2019-06-23 13:07:54 +0530

    update loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,41 +3,55 @@ import React from 'react';
 const axios = require('axios');
 
 const cmsUrl = process.env.REACT_APP_CMS_URL || 'http://192.168.0.37:8080';
-const path =  `${cmsUrl}/fullerton/?_format=json`;
+const path = `${cmsUrl}/fullerton/?_format=json`;
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+const loading_styles = {
+    'position' : 'absolute',
+    'left' : '50%',
+    'top' : '50%'
+}
 
 class Home extends React.Component {
     constructor() {
         super();
         this.state = {
+            loading: true,
             template: ''
         }
     }
 
     componentWillMount() {
-        axios.get(path, {
-            headers: {
-                'Access-Control-Allow-Origin' : '*'
-            }
+        this.setState({ loading: true }, () => {
+            axios.get(path, {
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
             })
-            .then(response => {
-                console.log(response.data);
-                
-                let htmlBody = response.data.body[0].value;
-                this.setState({
-                    template: htmlBody
-                })
-            })
-            .catch(err => {
-              console.log('ERROR::', err);
-            })
+                .then(response => this.setState({
+                    loading: false,
+                    template: response.data.body[0].value,
+                }));
+        });
     }
 
     render() {
-        return <div dangerouslySetInnerHTML={{__html: this.state.template}} />
-    }
+        // const { template, loading } = this.state;
+        return (
+            <div className='template'>
+                {/* <LoadingSpinner /> */}
+            { this.state.loading? <LoadingSpinner /> : <div  dangerouslySetInnerHTML={{ __html: this.state.template }} /> }
+            </div>
+        )
 }
+}
+
+const LoadingSpinner = () => (
+    <div style={loading_styles}>
+        <i className="fa fa-spinner fa-spin" />
+    </div>
+);
 
 export default Home;
